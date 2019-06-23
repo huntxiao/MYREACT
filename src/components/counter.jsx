b1c78b2c634a79aa0ff2e6e7146995cc5e5bc932
741929222 <77419292222qq.com>
2019-06-23 11:10:39 +0800

    Add a first new component

---
+++ src/components/counter.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor() {
+    super(); //子类构造方法需调用父构造方法
+    console.log("constructer", this);
+    this.handleIncrement = this.handleIncrement.bind(this); // 因为默认严格模式开启，绑定当前的this
+  }
+  state = {
+    count: 0,
+    imgurl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"]
+  };
+  styles = {
+    fontWeight: "bolder",
+    fontSize: "30px"
+  };
+  //   handleIncrement() {
+  //     console.log("Increment Clicked", this); //构造器实现绑定this
+  //   }
+  handleIncrement = () => {
+    console.log("Increment Clicked", this); //箭头函数不会重新绑定this
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleSubstract = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    // React.createElement("div");
+    return (
+      <React.Fragment>
+        <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <br />
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.handleSubstract}
+          className="btn btn-secondary btn-sm"
+        >
+          Substract
+        </button>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
+      </React.Fragment>
+    );
+  }
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>There are no tags</p>;
+    } else {
+      return (
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      );
+    }
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    console.dir(count);
+    return count === 0 ? "It's Zero now, fuck ya" : count;
+  }
+}
+
+export default Counter;
