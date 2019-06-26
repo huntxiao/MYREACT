5c0d342535e28cebc0f7d22df8b63259fe7d283a
huntxiao <huntxiao@gmail.com>
2019-06-26 23:49:11 +0800

    props learning

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,33 +1,29 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor() {
-    super(); //子类构造方法需调用父构造方法
-    console.log("constructer", this);
-    this.handleIncrement = this.handleIncrement.bind(this); // 因为默认严格模式开启，绑定当前的this
-  }
+  // constructor() {
+  //   super(); //子类构造方法需调用父构造方法
+  //   console.log("constructer", this);
+  //   this.handleIncrement = this.handleIncrement.bind(this); // 因为默认严格模式开启，绑定当前的this
+  // }
   state = {
-    count: this.props.value,
-    imgurl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
   styles = {
     fontWeight: "bolder",
     fontSize: "30px"
   };
-  //   handleIncrement() {
-  //     console.log("Increment Clicked", this); //构造器实现绑定this
-  //   }
+
   handleIncrement = product => {
     console.log("Increment Clicked", this); //箭头函数不会重新绑定this
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
   // doHandleIncrement = () => {
   //   this.handleIncrement({ id: 1 });
   // };
   handleSubstract = () => {
-    this.setState({ count: this.state.count - 1 });
+    this.setState({ value: this.state.value - 1 });
   };
   render() {
     // React.createElement("div");
@@ -44,33 +40,18 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
       </React.Fragment>
     );
   }
-  renderTags() {
-    if (this.state.tags.length === 0) {
-      return <p>There are no tags</p>;
-    } else {
-      return (
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      );
-    }
-  }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "It's Zero now, fuck ya" : count;
+    const { value } = this.state;
+    return value === 0 ? "It's Zero now" : value;
   }
 }
 
